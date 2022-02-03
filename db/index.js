dbb14e98ae08ed385c4cf629abf10a15b120b69a
sylverainv <sylverainv@gmail.com>
2022-02-03 06:44:53 -0500

    add dependencies

---
+++ db/index.js
@@ -0,0 +1,16 @@
+// List the dependencies here.
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+const util = require('util');
+
+// Create the connection to MySQL WorkBench
+let connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'employee_DB'
+});
+
+connection.query = util.promisify(connection.query);
